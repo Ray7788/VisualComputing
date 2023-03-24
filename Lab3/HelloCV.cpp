8c014457d12bd5e138f611a3776cfbfe6c944f00
Ray7788 <ray778@foxmail.com>
2023-03-24 17:32:17 +0000

    最终正确版本

--- Lab3/HelloCV.cpp
+++ Lab3/HelloCV.cpp
@@ -4,12 +4,13 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 
-using namespace cv;
+//using namespace cv;
 using namespace std;
 
-Mat bwImage;
-Mat thresh_BINARY;
-Mat thresh_OTSU;
+cv::Mat bwImage;
+cv::Mat thresh_BINARY;
+cv::Mat thresh_OTSU;
+
 int value = 128;
 int threshold_value = 0;
 char keyPress = 0;
@@ -19,13 +20,13 @@ string outputOTSUname;
 string outputBINARYname;
 
 static void Threshold_BINARY(int val, void*) {
-	cv::threshold(bwImage, thresh_BINARY, threshold_value, 255, THRESH_BINARY);
+	cv::threshold(bwImage, thresh_BINARY, threshold_value, 255, cv::THRESH_BINARY);
 	cv::imshow("Image Thresholding Binary", thresh_BINARY);
 }
 
 static void Threshold_OTSU(int val, void*) {
-	cv::threshold(bwImage, thresh_OTSU, threshold_value, 255, THRESH_OTSU);
-	cv:imshow("Image Thresholding OTSU", thresh_OTSU);
+	cv::threshold(bwImage, thresh_OTSU, threshold_value, 255, cv::THRESH_OTSU);
+	cv::imshow("Image Thresholding OTSU", thresh_OTSU);
 }
 
 int main(int argc, char* argv[]){
@@ -39,7 +40,7 @@ int main(int argc, char* argv[]){
 
 	// Load an image
 	// Takes the path as the first argument and some flags that determine how the data is interpreted as the second
-	cv::Mat image = imread(argv[1], IMREAD_UNCHANGED);
+	cv::Mat image = cv::imread(argv[1], cv::IMREAD_UNCHANGED);
 	if (image.empty()) {
 		printf("Can not load the image!\n");
 		return -1;
@@ -48,19 +49,18 @@ int main(int argc, char* argv[]){
 	string inputFilename = argv[1];
 	string outputOTSUname = inputFilename.substr(0, inputFilename.find_last_of(".")) + "_OTSU.png";
 	string outputBINARYname = inputFilename.substr(0, inputFilename.find_last_of(".")) + "_BINARY.png";
-	printf(outputBINARYname);
-
+	
 	// Create a display window to display it in
-	cv::namedWindow("Image Thresholding Binary", WINDOW_AUTOSIZE);
+	cv::namedWindow("Image Thresholding Binary", cv::WINDOW_AUTOSIZE);
 	// Another window for OTSU
-	cv::namedWindow("Image Thresholding OTSU", WINDOW_AUTOSIZE);
+	cv::namedWindow("Image Thresholding OTSU", cv::WINDOW_AUTOSIZE);
 
 	// Load the image into those windows
 	cv::imshow("Image Thresholding Binary", image);
 	cv::imshow("Image Thresholding OTSU", image);
 
 	// First convert it to an 8 - bit grey scale image
-	cv::cvtColor(image, bwImage, COLOR_BGR2GRAY, 0);
+	cv::cvtColor(image, bwImage, cv::COLOR_BGR2GRAY, 0);
 	// Add the track bar for Binary 
 	cv::createTrackbar("Threshold trackbar", "Image Thresholding Binary", &threshold_value, 255, Threshold_BINARY);
 
@@ -68,15 +68,11 @@ int main(int argc, char* argv[]){
 	Threshold_OTSU(0,0);
 	// Save the threshold image (OTSU)
 	cv::imwrite(outputOTSUname, thresh_OTSU);
-	cv::imwrite("bb.png", thresh_OTSU);
-
-	// Wait for the user to press a key in order to move on with the code
-	// cv::waitKey(0);
 
 	// Change the thresholding manually and save the image
 	while (keyPress != 'q') {
 		// Check for key press
-		keyPress = waitKey(1);
+		keyPress = cv::waitKey(1);
 
 		// Check if 's' key is pressed
 		if (keyPress == 's') {
@@ -85,9 +81,23 @@ int main(int argc, char* argv[]){
 
 		// Check if the image should be saved
 		if (saveImage) {
+			printf("Saving binary threshold image to: %s\n", outputBINARYname.c_str());
 			cv::imwrite(outputBINARYname, thresh_BINARY);
-			cv::imwrite("jj.png", thresh_BINARY);
-			printf("Image saved!\n");
+			if (cv::imread(outputBINARYname).empty()) {
+				printf("Failed to save binary threshold image!\n");
+			}
+			else {
+				printf("Binary threshold image saved successfully!\n");
+			}
+
+			printf("Saving OTSU threshold image to: %s\n", outputOTSUname.c_str());
+			cv::imwrite(outputOTSUname, thresh_OTSU);
+			if (cv::imread(outputOTSUname).empty()) {
+				printf("Failed to save OTSU threshold image!\n");
+			}
+			else {
+				printf("OTSU threshold image saved successfully!\n");
+			}
 			saveImage = false;
 		}
 	}
